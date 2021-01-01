0c3df88e03d582971bcfee50f6f883a8ef92ded5
paulday966 <paulday360@outlook.com>
2021-01-01 19:49:14 +0000

    Changed the latitude and longitude references for map center in function initMap()

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,8 +2,8 @@ function initMap() {
     let map = new google.maps.Map(document.getElementById("map"), {
         zoom: 10,
         center: {
-            lat: 51.237974,
-            lng: -0.436263
+            lat: 51.369621,
+            lng: -0.365670
         }
 });
 
