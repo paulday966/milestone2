375e6b9971c1fad16e6af750df36d18a95b72b77
paulday966 <paulday360@outlook.com>
2021-01-03 19:37:36 +0000

    Tried multiple attempts at creating an array and displaying data to index.html. Decided to use array objects and at the moment print to the console to make sure this works

--- assets/js/locationplaces.js
+++ assets/js/locationplaces.js
@@ -1,2 +1,27 @@
-let places = [ "Mandolay Hotel", "Hilton Cobham", "Painshill Park", "Guildford Castle", "Bookham Common", "Newlands Corner"];
+hotels = {
+    hotel1: {name: "Hotel Mandolay", location: "Guildford"},
+    hotel2: {name: "Hilton Cobham", location: "Cobham"},
+};
+
+placesofinterest = {
+    placeofinterest1: {name: "Polesden Lacy", location: "Cobham"},
+    placeofinterest2: {name: "Guildford Castle", location: "Guildford"},
+};
+
+placestoexplore = {
+    placetoexplore1: {name: "Bookham Common", location: "Great Bookham"},
+    placetoexplore2: {name: "Newlands Corner", location: "Guildford"},
+};
+
+for (let place in hotels) {
+    console.log(`${hotels[place].name}`);
+}
+
+for (let place in placesofinterest) {
+    console.log(`${placesofinterest[place].name}`);
+}
+
+for (let place in placestoexplore) {
+    console.log(`${placestoexplore[place].name}`);
+}
 
