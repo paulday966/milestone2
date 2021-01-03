8eee1f60cba7f358eccc2d47c5a6dc47adf3c8e6
paulday966 <paulday360@outlook.com>
2021-01-03 14:47:14 +0000

    Tried different approaches to show the google map markers when clicking on html buttons. Deleted the code I had added and decided to show all locations on the map. Returned the code to how I had it before this attempt and added the locations inside the locations array

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -10,8 +10,12 @@ function initMap() {
 let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 let locations = [
-    { lat: 51.330003, lng: -0.420852 }, /* Location A Painshill Park */
-    { lat: 51.234246, lng: -0.572456 } /* Location B Guildford Castle */
+    { lat: 51.239167, lng: -0.564511 }, /* Location A Mandolay Hotel */
+    { lat: 51.329200, lng: -0.411190 }, /* Location B Hilton Cobham */
+    { lat: 51.330003, lng: -0.420852 }, /* Location C Painshill Park */
+    { lat: 51.234246, lng: -0.572456 }, /* Location D Guildford Castle */
+    { lat: 51.292720, lng: -0.392500 }, /* Location E Bookham Common */
+    { lat: 51.232865, lng: -0.507144 } /* Location F Newlands Corner */
 ];
 
 let markers = locations.map(function(location, i) {
@@ -21,8 +25,9 @@ let markers = locations.map(function(location, i) {
     })
 });
 
+
 new MarkerClusterer(map, markers, {
     imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
 });
 
-}
+};
